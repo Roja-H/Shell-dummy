4809588677323ec96b12fc9924a6d8895606a52d
Roja-H <Rojahanumantharaju@gmail.com>
2025-12-15 04:25:16 +0530

    learning deeper

---
+++ 12-function.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR: please run this is in root user"
+    exit 1 #give anything other then 0 upto 127
+else
+    echo "you are running in root user"
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo "INSTALLING $2 SUCCESS"
+    else
+        echo "INSTALLING $2 is failure"
+        exit 1
+    fi
+}
+
+dnf list installed mysql -y
+if [ $? -ne 0 ]
+then
+    echo " mysql is not installed...going to install "
+    dnf install mysql -y
+    VALIDATE $? "mysql"
+else
+    echo " mysql is already installed ... nothing to do"
+fi
+
+nf list installed python3 -y
+if [ $? -ne 0 ]
+then
+    echo " python3 is not installed...going to install "
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else
+    echo " python3 is already installed ... nothing to do"
+fi
+
+nf list installed nginx -y
+if [ $? -ne 0 ]
+then
+    echo " nginx is not installed...going to install "
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else
+    echo " nginx is already installed ... nothing to do"
+fi
