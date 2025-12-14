95590074eeea3ce79b725ebc5dda24dfd4438a81
Roja-H <Rojahanumantharaju@gmail.com>
2025-12-15 02:55:31 +0530

    learning deeper

--- 8-special_variable.sh
+++ 8-special_variable.sh
@@ -3,10 +3,10 @@
 echo "All variables passed to the script : $@"
 echo "Number of variables : $#"
 echo "Script name : $0"
-echo "present working directory : $pwd"
-echo "home directory of user : $home"
-echo "which user is running script : $user
+echo "present working directory : $PWD"
+echo "home directory of user : $HOME"
+echo "which user is running script : $USER
 echo "PID of the script : $$
 sleep 10 &
 echo "PID of last command in background : $!"
-echo "number : $n"
+echo "exit status in shell scripting : $?"
