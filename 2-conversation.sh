27b566f2950ba9c725bebfdfe9034d8ab742970e
Roja-H <Rojahanumantharaju@gmail.com>
2025-12-14 23:11:55 +0530

    conversation started

---
+++ 2-conversation.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+echo "Roja: Hi soujanya how are you?"
+echo "Soujanya: hey, im great how about you"
+echo "Roja: not bad , currenly having some headache soujanya"
+echo "Soujanya: ohh sad.. take a nap roja you will be fine"
