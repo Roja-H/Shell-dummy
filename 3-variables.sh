ba0bc181c1385b950a0e7fe996adce6d49d8ca13
Roja-H <Rojahanumantharaju@gmail.com>
2025-12-14 23:16:51 +0530

    variables started

---
+++ 3-variables.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+ Person1: Roja
+ Person2: Soujanya
+
+echo "$Person1: Hi $Person2 how are you?"
+echo "$Person2: hey, im great how about you"
+echo "$Person1: not bad , currenly having some headache $Person2"
+echo "$Person2: ohh sad.. take a nap $Person1 you will be fine"
