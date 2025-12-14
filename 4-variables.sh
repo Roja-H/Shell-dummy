3c19cc73b99f407dbf35c441a202573542baa4cc
Roja-H <Rojahanumantharaju@gmail.com>
2025-12-14 23:37:59 +0530

    made some changes in variables

---
+++ 4-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+
+echo "$Person1: Hi $Person2 how are you?"
+echo "$Person2: hey, im great how about you"
+echo "$Person1: not bad , currenly having some headache $Person2"
+echo "$Person2: ohh sad.. take a nap $Person1 you will be fine"
