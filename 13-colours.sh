d39ee3c2444cc00b641e10add22504c21143cb6c
Roja-H <Rojahanumantharaju@gmail.com>
2025-12-15 18:43:02 +0530

    setting up colours

--- 13-colours.sh
+++ 13-colours.sh
@@ -4,6 +4,6 @@
 #yellow = 33
  #soo on upto 38
 
-
-echo -e "\e[31m hello"
+#here we have to end the colour at hello by (\e [0m) if not colour continues to next line as well
+echo -e "\e[31m hello \e[0m"
 echo "how are you"
