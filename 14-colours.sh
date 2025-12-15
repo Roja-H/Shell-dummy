6b9fe698df30338308414857816e575eedf4ac2a
Roja-H <Rojahanumantharaju@gmail.com>
2025-12-15 19:09:39 +0530

    setting up colours

---
+++ 14-colours.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo -e " $R ERROR: please run this is in root user $Y "
+    exit 1 #give anything other then 0 upto 127
+else
+    echo -e "you are running in root user $G" 
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo  -e "INSTALLING $2 SUCCESS $G"
+    else
+        echo -e "INSTALLING $2 is failure $R"
+        exit 1
+    fi
+}
+
+dnf list installed mysql -y
+if [ $? -ne 0 ]
+then
+    echo -e " mysql is not installed...going to install $Y "
+    dnf install mysql -y
+    VALIDATE $? "mysql"
+else
+    echo -e " mysql is already installed ... nothing to do $G"
+fi
+
+nf list installed python3 -y
+if [ $? -ne 0 ]
+then
+    echo -e " python3 is not installed...going to install  $Y"
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else
+    echo -e " python3 is already installed ... nothing to do $G"
+fi
