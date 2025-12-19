246c80096e93ccf7e78da1c89c9ef9bf3804afd0
Roja-H <Rojahanumantharaju@gmail.com>
2025-12-19 14:18:58 +0530

    hello world started

---
+++ 20.delete-old-logscopy.sh
@@ -0,0 +1,34 @@
+#!bin/bash
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOG_FOLDER="/var/logs/Shell-dummy-logs"
+SCRIPT_NAME="echo $0 | cut -d "." -f1
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log
+SOURCE_DIR="/home/ec2-user/app-logs"
+
+trap 'failure "${LINENO}" "${BASH_COMMAND}"' ERR
+
+if [ $USERID -ne 0]
+then
+    echo " please check and run the script in root user " 
+    exit 1
+else
+    echo " Script is running in root user" 
+fi
+
+
+echo " script started running at : $(date)
+
+log-file = $(find $SOURCE_DIR -name "*.log" -mtime +14)
+
+while read -r roja
+do
+  echo " deleting this log files $roja "
+  rm -rf $roja
+done<<$log-file
+
+echo "all worked successfully" 
