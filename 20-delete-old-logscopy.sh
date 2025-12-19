c551eb66b96b230cec11de430bffce7cf93d03ce
Roja-H <Rojahanumantharaju@gmail.com>
2025-12-19 15:48:10 +0530

    hello world started

--- 20-delete-old-logscopy.sh
+++ 20-delete-old-logscopy.sh
@@ -6,9 +6,9 @@ Y="\e[33m"
 N="\e[0m"
 
 LOG_FOLDER="/var/logs/Shell-dummy-logs"
-SCRIPT_NAME="echo $0 | cut -d "." -f1"
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log
-SOURCE_DIR="/home/ec2-user/app-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+SOURCE_DIR=/home/ec2-user/app-logs
 
 mkdir -p $LOGS_FOLDER
 
